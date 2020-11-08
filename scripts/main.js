70617b618dad72c065957c7fba8e9c69d974e854
Roman Schigolev <r.shchigolev2018@yandex.ru>
2020-11-08 13:24:50 +0700

    added a script for selecting checkboxes in the 'proximity to metro' filter

--- scripts/main.js
+++ scripts/main.js
@@ -10,4 +10,17 @@
   }
 
   filterButtons.forEach(filterButton => filterButton.addEventListener('click', toggleFilter));
+
+  const filterMetroCheckboxes = document.querySelectorAll(".filter__input--btn-enum");
+  const filterOptionCheckAny = document.querySelector("#filter__option-checkbox-any");
+  filterOptionCheckAny.addEventListener("click", () => {
+    filterMetroCheckboxes.forEach(filterMetroCheckbox => {
+      if (filterOptionCheckAny.checked) {
+        filterMetroCheckbox.disabled = true;
+        filterMetroCheckbox.checked = false;
+      } else {
+        filterMetroCheckbox.disabled = false;
+      }
+    })
+  })
 })();
